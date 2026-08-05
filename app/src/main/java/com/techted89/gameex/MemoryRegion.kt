f63763cd070ab34ad0ffafce447709094a90cdc6
ibr4himkh4li <ibrahimkolil777@gmail.com>
2026-08-06 00:15:39 +0600

    Fix TextInputLayout XML tag in view_scan_mode.xml

--- app/src/main/java/com/techted89/gameex/MemoryRegion.kt
+++
@@ -1,9 +0,0 @@
-package com.techted89.gameex
-
-data class MemoryRegion(
-    val startAddress: Long,
-    val endAddress: Long,
-    val size: Long,
-    val permissions: String,
-    val filename: String
-)
